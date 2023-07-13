7f61dd2b530bab1cfaf30520248257ea81baadcc
Dickerson, Dominick <92421959+Dominickator@users.noreply.github.com>
2023-07-13 14:05:14 -0400

    Update fedoraPreReboot.sh

--- fedoraPreReboot.sh
+++ fedoraPreReboot.sh
@@ -8,6 +8,7 @@ sudo sh -c 'echo "keepcache=True" >>/etc/dnf/dnf.conf'
 #Updating necessary components and restarting
 echo "Updating and rebooting... Run 'fedoraPostReboot.sh'"
 sudo dnf install dnf5
-sudo dnf5 update
 sudo dnf5 install cabextract curl dkms gnome-tweaks adw-gtk3-theme kgx
+sudo dnf5 update
+sudo dnf5 upgrade
 reboot
