007b7d3ef173dd9b4e6f5085cb8ab73c14e4b0e8
Dickerson, Dominick <92421959+Dominickator@users.noreply.github.com>
2024-02-05 09:31:01 -0600

    Update fedoraPreReboot.sh

--- fedoraPreReboot.sh
+++ fedoraPreReboot.sh
@@ -7,18 +7,18 @@ sudo sh -c 'echo "keepcache=True" >>/etc/dnf/dnf.conf'
 
 #Installing gnome console
 echo "Installing GNOME Console..."
-sudo dnf install gnome-console
+sudo dnf install gnome-console --assumeyes
 
 #Updating necessary components and restarting
 echo "Updating and rebooting... Run 'fedoraPostReboot.sh'"
 sudo systemctl disable NetworkManager-wait-online.service
-sudo dnf install dnf5
-sudo dnf5 install cabextract curl dkms gnome-tweaks adw-gtk3-theme kgx
+sudo dnf install dnf5 --assumeyes
+sudo dnf5 install cabextract curl dkms gnome-tweaks adw-gtk3-theme kgx --assumeyes
 sudo dnf copr enable trixieua/mutter-patched -y && sudo dnf update --refresh
 sudo mount /dev/nvme0n1p3 /mnt
 cd /mnt/Windows/Fonts
 sudo cp -r * /usr/share/fonts
 sudo fc-cache -fv
-sudo dnf5 update
-sudo dnf5 upgrade
+sudo dnf5 update --assumeyes
+sudo dnf5 upgrade --assumeyes
 reboot
